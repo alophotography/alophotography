ad0b044e3c0076a38695b324991d6999163b9418
Annie Lo <annielo0822@gmail.com>
2018-01-14 21:56:36 -0800

    Upload photos: mamiyac330s-2berk

--- src/app/data/CollectionsData.js
+++ src/app/data/CollectionsData.js
@@ -1,5 +1,8 @@
 import collec1ph1 from '../assets/photos/mamiyac330s-1sh/1sh-1.jpg';
 import collec1ph2 from '../assets/photos/mamiyac330s-1sh/1sh-2.jpg';
+import collec2ph1 from '../assets/photos/mamiyac330s-2berk/2berk-1.jpg';
+import collec2ph2 from '../assets/photos/mamiyac330s-2berk/2berk-2.jpg';
+import collec2ph3 from '../assets/photos/mamiyac330s-2berk/2berk-3.jpg';
 
 export const CollectionsData = [
   {
@@ -14,7 +17,7 @@ export const CollectionsData = [
         date: '',
         location: 'Shanghai, China',
         collection: 'collection-1',
-        camera: 'mamiyac300s',
+        camera: 'mamiyac330s',
         film: 'Kodak Portra 400',
         format: '120'
       },
@@ -24,7 +27,45 @@ export const CollectionsData = [
         date: '',
         location: 'Shanghai, China',
         collection: 'collection-1',
-        camera: 'mamiyac300s',
+        camera: 'mamiyac330s',
+        film: 'Kodak Portra 400',
+        format: '120'
+      }
+    ]
+  },
+  {
+    id: 'collection-2',
+    title: 'Berkeley',
+    date: '',
+    link: '',
+    photos: [
+      {
+        id: '1',
+        img: collec2ph1,
+        date: '',
+        location: 'Berkeley, CA',
+        collection: 'collection-2',
+        camera: 'mamiyac330s',
+        film: 'Kodak Portra 400',
+        format: '120'
+      },
+      {
+        id: '2',
+        img: collec2ph2,
+        date: '',
+        location: 'Berkeley, CA',
+        collection: 'collection-2',
+        camera: 'mamiyac330s',
+        film: 'Kodak Portra 400',
+        format: '120'
+      },
+      {
+        id: '3',
+        img: collec2ph3,
+        date: '',
+        location: 'Berkeley, CA',
+        collection: 'collection-2',
+        camera: 'mamiyac330s',
         film: 'Kodak Portra 400',
         format: '120'
       }
