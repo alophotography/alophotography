e454c2cb0295821c0e43b053b553f69e05760e0e
Annie Lo <annielo0822@gmail.com>
2018-01-28 17:39:39 -0800

    Add camera info to Camera page

--- src/app/components/cameras/Camera.js
+++ src/app/components/cameras/Camera.js
@@ -16,21 +16,18 @@ class Camera extends Component {
 
   render() {
     const listView = (
-      <div className="container-fluid center-container">
-        <div className="camera-title">{this.data.title}</div>
-        <div className="photos-container">
-          {
-            this.data.photos.map((photo, i) => {
-              return (
-                <Photo
-                  key={i}
-                  data={photo}
-                  view={'camera'}
-                />
-              )
-            })
-          }
-        </div>
+      <div className="photos-container">
+        {
+          this.data.photos.map((photo, i) => {
+            return (
+              <Photo
+                key={i}
+                data={photo}
+                view={'camera'}
+              />
+            )
+          })
+        }
       </div>
     );
     const gridView = (
@@ -39,7 +36,24 @@ class Camera extends Component {
         columns={COLUMNS}
       />
     );
-    return gridView;
+    return (
+      <div className="container-fluid center-container">
+        <div className="camera-info-container">
+          <div className="camera-img-container">
+            <img
+              className="camera-img"
+              src={this.data.cover}
+              alt={this.data.title}
+            />
+          </div>
+          <div className="camera-details-container">
+            <div className="camera-title">{this.data.title}</div>
+            <div className="camera-info">{this.data.info}</div>
+          </div>
+        </div>
+        {gridView}
+      </div>
+    );
   }
 }
 
--- src/app/data/CamerasData.js
+++ src/app/data/CamerasData.js
@@ -19,13 +19,25 @@ export const CamerasData = [
     title: CAMERAS.MAMIYA_C330S,
     date: '',
     cover: 'https://i.imgur.com/K8NKvci.jpg?1',
-    photos: mamiyaC330s
+    photos: mamiyaC330s,
+    info:
+      'Camera type: Twin-lens reflex\n' +
+      'Film format: Medium format (6x6)\n' +
+      'Lens: Mamiya-Sekor 1:3.5 f = 65mm\n' +
+      'Made in Japan\n' +
+      'Released in 1983'
   },
   {
     id: CAMERAS.MINOLTA_X700,
     title: CAMERAS.MINOLTA_X700,
     date: '',
-    cover: '',
-    photos: minoltaX700
+    cover: 'https://i.imgur.com/K8NKvci.jpg?1',
+    photos: minoltaX700,
+    info:
+      'Camera type: Single-lens reflex\n' +
+      'Film format: 35mm\n' +
+      'Lens: Minolta MD 50mm 1:2\n' +
+      // 'Made in Japan\n' +
+      'Released in 1981'
   }
 ]
